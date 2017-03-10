2e395ffa55cd362894e6c4c38e8079d59a099b29
Len Johnson <radixmo@gmail.com>
2017-03-10 20:13:19 +0000

    fixed incorrect text_domain

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -4,14 +4,14 @@
  *
  * Eventually, some of the functionality here could be replaced by core features.
  *
- * @package freeshop
+ * @package understrap
  */
 
-if ( ! function_exists( 'freeshop_posted_on' ) ) :
+if ( ! function_exists( 'understrap_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time and author.
  */
-function freeshop_posted_on() {
+function understrap_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 	$updated_on = '';
 	$updated_string = '';
@@ -34,11 +34,11 @@ function freeshop_posted_on() {
 		);
 	}
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'freeshop' ),
+		esc_html_x( 'Posted on %s', 'post date', 'understrap' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'freeshop' ),
+		esc_html_x( 'by %s', 'post author', 'understrap' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
@@ -49,33 +49,33 @@ function freeshop_posted_on() {
 }
 endif;
 
-if ( ! function_exists( 'freeshop_entry_footer' ) ) :
+if ( ! function_exists( 'understrap_entry_footer' ) ) :
 /**
  * Prints HTML with meta information for the categories, tags and comments.
  */
-function freeshop_entry_footer() {
+function understrap_entry_footer() {
 	// Hide category and tag text for pages.
 	if ( 'post' === get_post_type() ) {
 		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'freeshop' ) );
-		if ( $categories_list && freeshop_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'freeshop' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+		$categories_list = get_the_category_list( esc_html__( ', ', 'understrap' ) );
+		if ( $categories_list && understrap_categorized_blog() ) {
+			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'understrap' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 		}
 		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'freeshop' ) );
+		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'understrap' ) );
 		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'freeshop' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'understrap' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 		}
 	}
 	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 		echo '<span class="comments-link">';
-		comments_popup_link( esc_html__( 'Leave a comment', 'freeshop' ), esc_html__( '1 Comment', 'freeshop' ), esc_html__( '% Comments', 'freeshop' ) );
+		comments_popup_link( esc_html__( 'Leave a comment', 'understrap' ), esc_html__( '1 Comment', 'understrap' ), esc_html__( '% Comments', 'understrap' ) );
 		echo '</span>';
 	}
 	edit_post_link(
 		sprintf(
 			/* translators: %s: Name of current post */
-			esc_html__( 'Edit %s', 'freeshop' ),
+			esc_html__( 'Edit %s', 'understrap' ),
 			the_title( '<span class="screen-reader-text">"', '"</span>', false )
 		),
 		'<span class="edit-link">',
@@ -89,8 +89,8 @@ endif;
  *
  * @return bool
  */
-function freeshop_categorized_blog() {
-	if ( false === ( $all_the_cool_cats = get_transient( 'freeshop_categories' ) ) ) {
+function understrap_categorized_blog() {
+	if ( false === ( $all_the_cool_cats = get_transient( 'understrap_categories' ) ) ) {
 		// Create an array of all the categories that are attached to posts.
 		$all_the_cool_cats = get_categories( array(
 			'fields'     => 'ids',
@@ -100,7 +100,7 @@ function freeshop_categorized_blog() {
 		) );
 		// Count the number of categories that are attached to the posts.
 		$all_the_cool_cats = count( $all_the_cool_cats );
-		set_transient( 'freeshop_categories', $all_the_cool_cats );
+		set_transient( 'understrap_categories', $all_the_cool_cats );
 	}
 	if ( $all_the_cool_cats > 1 ) {
 		// This blog has more than 1 category so components_categorized_blog should return true.
@@ -112,15 +112,15 @@ function freeshop_categorized_blog() {
 }
 
 /**
- * Flush out the transients used in freeshop_categorized_blog.
+ * Flush out the transients used in understrap_categorized_blog.
  */
-function freeshop_category_transient_flusher() {
+function understrap_category_transient_flusher() {
 	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 		return;
 	}
 	// Like, beat it. Dig?
-	delete_transient( 'freeshop_categories' );
+	delete_transient( 'understrap_categories' );
 }
-add_action( 'edit_category', 'freeshop_category_transient_flusher' );
-add_action( 'save_post',     'freeshop_category_transient_flusher' );
+add_action( 'edit_category', 'understrap_category_transient_flusher' );
+add_action( 'save_post',     'understrap_category_transient_flusher' );
 
